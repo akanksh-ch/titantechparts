5581952c8cfb644a2d3653f50e51dc1da4f322c3
Akanksh Chitimalla <144524423+akanksh-ch@users.noreply.github.com>
2026-02-12 01:10:40 +0000

    Added images as static files

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers import auth, users, inventory, orders
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(title="TitanTechParts Backend")
+
+app.mount("/images", StaticFiles(directory="images"), name="images")
 
 origins = [
     "http://localhost:3000",
