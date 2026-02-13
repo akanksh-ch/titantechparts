4c71204b406e58e0dfc0c4ad8d1ddc57d9f2aa74
Akanksh Chitimalla <144524423+akanksh-ch@users.noreply.github.com>
2026-02-13 14:22:48 +0000

    fix(db): add certifi CA bundle for ssl connection

--- app/database.py
+++ app/database.py
@@ -4,10 +4,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+import certifi
+
 MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
 DATABASE_NAME = "titantechparts"
 
-client = AsyncIOMotorClient(MONGO_URL)
+client = AsyncIOMotorClient(MONGO_URL, tlsCAFile=certifi.where())
 db = client[DATABASE_NAME]
 
 async def get_database():
