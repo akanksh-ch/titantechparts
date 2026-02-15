c25bd276a38a8250a07103b9f80a86d71d988120
Akanksh Chitimalla <144524423+akanksh-ch@users.noreply.github.com>
2026-02-15 22:32:08 +0000

    Using localhost and prod where needed

--- app/database.py
+++ app/database.py
@@ -9,7 +9,7 @@ import certifi
 MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
 DATABASE_NAME = "titantechparts"
 
-client = AsyncIOMotorClient(MONGO_URL, tlsCAFile=certifi.where())
+client = AsyncIOMotorClient(MONGO_URL, tlsCAFile=certifi.where()) if "localhost" not in MONGO_URL and "127.0.0.1" not in MONGO_URL else AsyncIOMotorClient(MONGO_URL)
 db = client[DATABASE_NAME]
 
 async def get_database():
